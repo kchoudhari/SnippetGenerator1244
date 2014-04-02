39414cb970d61e5c9f6df43173deedef468118cd
Koustubh <kc@kisi.de>
2014-04-02 17:53:07 -0400

    Google Api

--- src/main/java/SnippetGenerator/SnippetServlet.java
+++ src/main/java/SnippetGenerator/SnippetServlet.java
@@ -23,34 +23,42 @@ import java.util.Map;
 
 @MultipartConfig
 public class SnippetServlet extends HttpServlet {
-    HashMap<URL, String> dummyData=new HashMap<URL, String>();
+    HashMap<String, HashMap<String, String>> dummyData = new HashMap<String, HashMap<String, String>>();
     ObjectMapper mapper = new ObjectMapper();
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         response.setContentType("text/event-stream");
         response.setCharacterEncoding("UTF-8");
         //Stack<Object> recentUpdates=helper.getUpdates();
         //int size=recentUpdates.size();
 
 
-
         PrintWriter writer = response.getWriter();
-        String query= request.getParameter("msg1");
+        String query = request.getParameter("msg1");
 
-        System.out.println("hi");
-        data dummy=new data(query);
-        dummyData= dummy.getMap();
+        data dummy = new data(query);
+        dummyData = dummy.getMap();
 
-        Iterator iterator=dummyData.entrySet().iterator();
+        Iterator iterator = dummyData.entrySet().iterator();
 
-        while(iterator.hasNext()){
-            Map.Entry pairs = (Map.Entry)iterator.next();
-            writer.println("<div class='row-fluid post-river-twocolumn border-bottom id-5294f1e5ecad046801c97170 featurepost'>" +
-                    "<a href='"+pairs.getKey()+"' target='_blank'>"+pairs.getKey()+"</a>" +
-                    "<br/>"+pairs.getValue()+"</div>"
-            );
-            writer.flush();
+        while (iterator.hasNext()) {
+            Map.Entry pairs = (Map.Entry) iterator.next();
+            Map<String, String> innerMap = new HashMap<String, String>();
+            innerMap = (Map)pairs.getValue();
+            Iterator iterator1 = innerMap.entrySet().iterator();
+
+            while (iterator1.hasNext()) {
+                Map.Entry innerPairs = (Map.Entry) iterator1.next();
+
+                writer.println("<div class='row-fluid post-river-twocolumn border-bottom id-5294f1e5ecad046801c97170 featurepost'>" +
+                                "<a href='" + pairs.getKey() + "' target='_blank'>" + innerPairs.getKey() + "</a>" +
+                                "<br/>" + innerPairs.getValue() + "</div>"
+                );
+                writer.flush();
+            }
+
         }
 
         writer.close();
@@ -63,4 +71,11 @@ public class SnippetServlet extends HttpServlet {
         System.out.println("posting");
     }
 
-    }
+}
+
+
+
+
+
+
+
--- src/main/java/SnippetGenerator/data.java
+++ src/main/java/SnippetGenerator/data.java
@@ -1,16 +1,17 @@
 package SnippetGenerator;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.customsearch.Customsearch;
+import com.google.api.services.customsearch.model.Result;
+import com.google.api.services.customsearch.model.Search;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,11 +23,11 @@ import java.util.Map;
  */
 public class data {
 
-    HashMap<Object, String> data = new HashMap<Object, String>();
+    HashMap<Object, HashMap<String, String>> data = new HashMap<Object, HashMap<String, String>>();
 
     data(String query) {
 
-        data = (HashMap<Object, String>) JSONParser(query);
+        data = (HashMap<Object, HashMap<String, String>>) Parser(query);
 
     }
 
@@ -34,40 +35,43 @@ public class data {
         return data;
     }
 
-    Map JSONParser(String query) {
-        Map<Object, String> genData=new HashMap<Object, String>();
+    Map Parser(String query) {
+        Map<String, Map<String, String>> genData = new HashMap<String, Map<String, String>>();
+
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader("/Users/harsh/IdeaProjects/SnippetGenerator/src/main/java/data.json"));
-            JSONObject jsonObject = (JSONObject) obj;
 
-            String cricketString = String.valueOf((JSONObject) jsonObject.get(query));
+            // Get your query string posted by the user
+            //  String query = request.getParameter("query");
+            //Instantiate a Customsearch object using NetHttpTransport and the JacksonFactory (JSON library)
+            HttpRequestInitializer httpRequestInitializer = new HttpRequestInitializer() {
+                @Override
+                public void initialize(HttpRequest httpRequest) throws IOException {
 
-            System.out.println(cricketString);
+                }
+            };
+            System.out.println("hi");
+            Customsearch customsearch = new Customsearch(new NetHttpTransport(), new JacksonFactory(), httpRequestInitializer);
+            // Instantiate a Customsearch.Cse.List object using your query string
+            com.google.api.services.customsearch.Customsearch.Cse.List list = customsearch.cse().list(query);
+            // Set your API KEY
+            list.setKey("AIzaSyCNW_RkKOLUdKMtcszzibzALEkoHazrJyw");
+            // Set your custom search engine ID
+            list.setCx("016346005891615725002:bgiac4u5emc");
+            // Execute the search
+            Search results = list.execute();
+            // Get the result items
 
-            // convert user object to json string, and save to a file
-            genData = mapper.readValue(String.valueOf(cricketString).replace("\\", ""), Map.class);
-            // display to console
-
-
-        } catch (JsonGenerationException e) {
-
-            e.printStackTrace();
-
-        } catch (JsonMappingException e) {
-
-            e.printStackTrace();
-
+            List<Result> items = results.getItems();
+            // Loop through your result items and stream them to the client
+            for (Result result : items) {
+                Map<String, String> innerMap = new HashMap<String, String>();
+                innerMap.put(result.getTitle(), result.getSnippet());
+                genData.put(result.getLink(), innerMap);
+            }
         } catch (IOException e) {
-
             e.printStackTrace();
-
-        } catch (ParseException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
         return genData;
+
     }
 }
-
-
